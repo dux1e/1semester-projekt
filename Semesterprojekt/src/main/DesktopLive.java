a857a23759819913ff1e564446668ba184ed8864
Joakim <joak0451@gmail.com>
2020-11-26 22:35:55 +0100

    live test i klasse fra desktop
    Dette er en test i samme klasse på samme linje, fra min desktop :)

--- Semesterprojekt/src/main/DesktopLive.java
+++ Semesterprojekt/src/main/DesktopLive.java
@@ -3,4 +3,8 @@ package main;
 public class DesktopLive {
 //	Dette er en kommentar for at se hvordan det påvirker programmet
 //	Når jeg laver to nye klasser på samme tid.
+	
+	public static void desktopLiveTestAgain() {
+		System.out.println("Jeg sidder på min desktop og tester");
+	}
 }
