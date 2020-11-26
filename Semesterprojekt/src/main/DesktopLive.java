a5f18cb4e88aa0ed2434cc566aa9d1cca5709b40
Joakim <joak0451@gmail.com>
2020-11-26 22:35:10 +0100

    live i samme klasse
    Dette er en test i samme klasse på samme linje, for at se hvordan det kommer til at virke

--- Semesterprojekt/src/main/DesktopLive.java
+++ Semesterprojekt/src/main/DesktopLive.java
@@ -3,4 +3,8 @@ package main;
 public class DesktopLive {
 //	Dette er en kommentar for at se hvordan det påvirker programmet
 //	Når jeg laver to nye klasser på samme tid.
+	
+	public static void testLiveAgain() {
+		System.out.println("Jeg sider nu i den samme klasse og tester lige");
+	}
 }
