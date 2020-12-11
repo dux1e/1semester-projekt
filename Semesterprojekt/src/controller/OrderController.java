eb69ec5542e34456bacbb50fc14d5264f828db73
Rasmus-larsen <rasmus-larsen1999@hotmail.dk>
2020-12-11 12:57:54 +0100

    CreateOrder og calculateTotals
    Disse er fikset og oprettet

--- Semesterprojekt/src/controller/OrderController.java
+++ Semesterprojekt/src/controller/OrderController.java
@@ -1,23 +1,16 @@
 package controller;
-import java.util.*;
 import model.*;
 
 public class OrderController {
 	private OrderContainer orderContainer;
 	private EmployeeController employeeController;
 	private ProductController productController;
-	private List<OrderLine> orderLines;
-	private double netTotal;
-	private double vatTotal;
 	private Order currentOrder;
 
 	public OrderController() {
 		this.orderContainer = OrderContainer.getInstance();
 		this.employeeController = new EmployeeController();
 		this.productController = new ProductController();
-		this.orderLines = new ArrayList<>();
-		this.netTotal = 0;
-		this.vatTotal = 0;
 		this.currentOrder = null;
 	}
 	
@@ -29,25 +22,6 @@ public class OrderController {
 	public void addProduct(int barcode, int quantity) {
 		Product product = productController.findProductByBarcode(barcode);
 		OrderLine ol = new OrderLine(product, quantity);
-		orderLines.add(ol);
-		calculateTotals();
-	}
-	
-	private void calculateTotals() {
-		calculateNetTotal();
-		calculateVatTotal();
-	}
-	
-	private double calculateNetTotal() {
-		netTotal = 0;
-		for(int i = 0; i<orderLines.size(); i++) {
-			netTotal += orderLines.get(i).getSubTotal();
-		}
-		return netTotal;
-	}
-	
-	private double calculateVatTotal() {
-		vatTotal = netTotal * 1.25;
-		return vatTotal;
+		this.currentOrder.addOrderLine(ol);
 	}
 }
--- Semesterprojekt/src/model/Order.java
+++ Semesterprojekt/src/model/Order.java
@@ -23,53 +23,72 @@ public class Order {
 		this.employee = e;
 	}
 	
+	public void addOrderLine(OrderLine ol) {
+		orderLines.add(ol);
+		calculateTotals();
+	}
+	
+	private double calculateTotals() {
+		double total = calculateNetTotal() + calculateVatTotal();
+		return total;
+	}
+	
+	private double calculateNetTotal() {
+		netTotal = 0;
+		for(int i = 0; i < orderLines.size(); i++) {
+			netTotal += orderLines.get(i).getSubTotal();
+		}
+		return netTotal;
+	}
+	
+	private double calculateVatTotal() {
+		vatTotal = netTotal * 1.25;
+		return vatTotal;
+	}
+
+	
 	// getters and setters below
 	public int getOrderNo() {
 		return orderNo;
 	}
+
 	public void setOrderNo(int orderNo) {
 		this.orderNo = orderNo;
 	}
-	
+
 	public String getProject() {
 		return project;
 	}
-	
+
 	public void setProject(String project) {
 		this.project = project;
 	}
-	
-	public double getVatTotal() {
-		return vatTotal;
-	}
-	
-	public void setVatTotal(double vatTotal) {
-		this.vatTotal = vatTotal;
-	}
-	
+
 	public double getNetTotal() {
 		return netTotal;
 	}
-	
-	public void setNetTotal(double netTotal) {
-		this.netTotal = netTotal;
+
+	public double getVatTotal() {
+		return vatTotal;
 	}
-	
+
 	public List<OrderLine> getOrderLines() {
 		return orderLines;
 	}
-	
-	public void setOrderLines(List<OrderLine> orderLines) {
-		this.orderLines = orderLines;
-	}
-	
+
 	public Customer getCustomer() {
 		return customer;
 	}
-	
+
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	
 
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
 }
