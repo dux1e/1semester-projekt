ffb72d40e9bf1cae64f2651a57b0fc80b650b896
Joakim <joak0451@gmail.com>
2020-12-11 12:17:10 +0100

    Det ser godt ud nu
    Ja, det ser godt ud

--- Semesterprojekt/src/model/OrderLine.java
+++ Semesterprojekt/src/model/OrderLine.java
@@ -1,35 +1,41 @@
 package model;
 
 public class OrderLine {
-private int quantity;
-private double subTotal;
-private Product product;
-public OrderLine(Product product, int quantity) {
-	this.quantity = quantity;
-	subTotal = calculateSubTotal();
-	this.product = product;
-}
-public int getQuantity() {
-	return quantity;
-}
-public void setQuantity(int quantity) {
-	this.quantity = quantity;
-	subTotal = calculateSubTotal();
-}
-public Product getProduct() {
-	return product;
-}
-public void setProduct(Product product) {
-	this.product = product;
-	subTotal = calculateSubTotal();
-}
+	private int quantity;
+	private double subTotal;
+	private Product product;
+	
+	public OrderLine(Product product, int quantity) {
+		this.quantity = quantity;
+		subTotal = calculateSubTotal();
+		this.product = product;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+		subTotal = calculateSubTotal();
+	}
+	
+	public Product getProduct() {
+		return product;
+	}
+	
+	public void setProduct(Product product) {
+		this.product = product;
+		subTotal = calculateSubTotal();
+	}
 
-public double getSubTotal() {
-	return subTotal;
-}
-public double calculateSubTotal() {
-	subTotal = quantity * product.getPrice();
-	return subTotal;
-}
+	public double getSubTotal() {
+		return subTotal;
+	}
+	
+	public double calculateSubTotal() {
+		subTotal = quantity * product.getPrice();
+		return subTotal;
+	}
 
 }
